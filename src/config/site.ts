b89d705023a95ae28108958d605acd9c147b5643
Wupxy <muhammad.122140193@student.itera.ac.id>
2024-10-16 19:13:56 +0700

    set up prisma ORM

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,17 +3,17 @@ import { SiteConfig } from "@/types"
 import { env } from "@/env.mjs"
 
 export const siteConfig: SiteConfig = {
-  name: "Next Entree",
-  author: "redpangilinan",
+  name: "Posyandu Marga Agung",
+  author: "Team KSI Marga Agung",
   description:
-    "Next.js 14+ starter template with app router, shadcn/ui, typesafe env, icons and configs setup.",
+    "Posyandu Marga Agung adalah aplikasi yang digunakan untuk mencatat data posyandu di Marga Agung",
   keywords: ["Next.js", "React", "Tailwind CSS", "Radix UI", "shadcn/ui"],
   url: {
     base: env.NEXT_PUBLIC_APP_URL,
-    author: "https://rdev.pro",
+    author: "",
   },
   links: {
-    github: "https://github.com/redpangilinan/next-entree",
+    github: "",
   },
   ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.jpg`,
 }
