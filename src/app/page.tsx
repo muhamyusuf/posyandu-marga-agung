1c608e8cbde5a44f29b58b332c3fc1f2b747f5cd
Wupxy <muhammad.122140193@student.itera.ac.id>
2024-10-16 19:28:04 +0700

    add input and label components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import { Icons } from "@/components/icons"
 import { ModeToggle } from "@/components/mode-toggle"
 
@@ -26,6 +28,25 @@ export default function Home() {
             Get Started
           </Link>
           <ModeToggle />
+        </div>
+
+        <h1 className="text-4xl font-semibold">Contoh Penggunaan</h1>
+
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col items-start justify-center gap-1">
+            <Label>NIK</Label>
+            <Input />
+          </div>
+
+          <div className="flex flex-col items-start justify-center gap-1">
+            <Label>Nama</Label>
+            <Input />
+          </div>
+
+          <div className="flex gap-2 self-end">
+            <Button variant={"outline"}>Cancel</Button>
+            <Button>Submit</Button>
+          </div>
         </div>
       </div>
     </main>
