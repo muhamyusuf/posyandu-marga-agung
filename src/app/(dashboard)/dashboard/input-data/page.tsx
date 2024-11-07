5eef9d5069c0e221a147d1d45a02291975c7f7b5
rayhanfatihg <61815053+rayhanfatihg@users.noreply.github.com>
2024-11-07 19:19:54 +0700

    form input-data

--- src/app/(dashboard)/dashboard/input-data/page.tsx
+++ src/app/(dashboard)/dashboard/input-data/page.tsx
@@ -15,6 +15,22 @@ export default function InputDataPage() {
             <h1 className="text-xl font-semibold">DATA LAYANAN KELUARGA</h1>
           </div>
 
+          {/* Dropdown untuk memilih layanan */}
+          <div className="mb-4">
+            <label className="mb-1 block text-sm font-semibold">
+              Tampilan Data Tabel
+            </label>
+            <select className="w-full rounded-md border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500">
+              <option value="">-Pilih-</option>
+              <option value="ibu_hamil">Layanan Ibu Hamil</option>
+              <option value="remaja_putri">Layanan Remaja Putri</option>
+              <option value="calon_pengantin">Layanan Calon Pengantin</option>
+              <option value="keluarga">Layanan Keluarga</option>
+              <option value="anak">Layanan Anak</option>
+              <option value="lansia">Layanan Lansia</option>
+            </select>
+          </div>
+
           {/* Form */}
           <form>
             {/* Nama Lengkap Kepala Keluarga */}
